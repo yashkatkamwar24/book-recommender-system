b2adca045358d7d741710e38a02ee05fb931805e
Yash Katkamwar <yashkatkamwar1@gmail.com>
2026-08-16 20:56:32 +0530

    Build TF-IDF book recommendation engine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # print("Book Recommender System")
 from fastapi import FastAPI, HTTPException
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 
 app = FastAPI()
@@ -11,9 +12,34 @@ books["combined_text"] = books["genre"] + " " + books["description"]
 
 vectorizer = TfidfVectorizer()
 tfidf_matrix = vectorizer.fit_transform(books["combined_text"])
-print(tfidf_matrix.shape)
 
-print(books[["title", "combined_text"]])
+similarity_matrix = cosine_similarity(tfidf_matrix)
+# print(similarity_matrix.shape)
+# print(books[["title", "combined_text"]])
+
+def recommend_books(book_title):
+    # Find the index of the selected books
+    book_index = books[books["title"].str.lower() == book_title.lower()].index[0]
+
+    # Get similarity score for the selected book
+    similarity_scores = similarity_matrix[book_index]
+
+    # Create pairs of (index, similarity score)
+    book_scores = list(enumerate(similarity_scores))
+
+    # Sort by similarity score from highest to lowest
+    book_scores = sorted(book_scores, key=lambda x: x[1], reverse=True)
+
+    # Get the top 5 similar books, excluding the selected book
+    recommendation = []
+
+    for index, score in book_scores[1:6]:
+        recommendation.append(books.iloc[index]["title"])
+
+    return recommendation
+
+print(recommend_books("The Hobbit"))
+
 
 @app.get("/")
 def home():
